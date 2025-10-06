ace1255cc810436e20bb0bd2c86d011af51edd95
sharkfishjelly <polygoNight@gmail.com>
2025-10-06 21:01:59 +0900

    Update models and Rename apps → app

--- apps/diary/app.py
+++ apps/diary/app.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class DiaryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.diary'
+    name = 'app.diary'
--- myproject/settings.py
+++ myproject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.diary',
+    'apps.diary.app.DiaryConfig'
 ]
 
 MIDDLEWARE = [
